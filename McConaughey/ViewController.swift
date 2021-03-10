157c24a262102cbeedd5164870740045a348d8ba
Jacob P Wang <59838504+jacobwang7@users.noreply.github.com>
2021-03-10 18:25:51 -0500

    Funny app that controls how alright Matthew McConaughey is

--- McConaughey/ViewController.swift
+++ McConaughey/ViewController.swift
@@ -6,14 +6,40 @@
 //
 
 import UIKit
+import AVFoundation
+
+var audioPlayer: AVAudioPlayer!
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var numberLabel: UILabel!
+    @IBOutlet weak var stepper: UIStepper!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        numberLabel.text = "\(Int(stepper.value))"
     }
-
-
+    
+    @IBAction func stepperPressed(_ sender: UIStepper) {
+        numberLabel.text = "\(Int(stepper.value))"
+    }
+    
+    
+    @IBAction func tellMeButton(_ sender: UIButton) {
+        playSound(name: "alright", numberOfloops: Int(stepper.value))
+    }
+    
 }
 
+func playSound(name: String, numberOfloops: Int) {
+    if let sound = NSDataAsset(name: name) {
+        do {
+            try audioPlayer = AVAudioPlayer(data: sound.data)
+            audioPlayer.numberOfLoops = numberOfloops - 1
+            audioPlayer.play()
+        } catch {
+            print("😡 ERROR: \(error.localizedDescription) Could not initialize AVAudioPlayer object.")
+        }
+    } else {
+        print("😡 ERROR: Could not read data from file sound0")
+    }
+}
